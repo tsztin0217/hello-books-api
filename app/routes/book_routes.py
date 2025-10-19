680f5c51ee67cf19958a9e4e40eb2c6db0962bff
Tsz Tin Vivian Soo <tsztin0217@gmail.com>
2025-10-19 13:56:24 -0400

    Add book retrieval endpoints and validation logic

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 from app.models.book import books
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
@@ -14,3 +14,27 @@ def get_all_books():
             }
         )
     return {"books": books_response}
+
+@books_bp.get("/<book_id>")
+def get_one_book(book_id):
+    book = validate_book(book_id)
+
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+    }
+
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        response = {"message": f"book {book_id} invalid"}
+        abort(make_response(response, 400))
+        
+    for book in books:
+        if book.id == book_id:
+            return book
+    
+    response = {"message": f"book {book_id} not found"}
+    abort(make_response(response, 404))
