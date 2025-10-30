4bfcdbd8993cbb8595207b147a83ea3c650227ed
Tsz Tin Vivian Soo <tsztin0217@gmail.com>
2025-10-30 01:19:24 -0400

    Add fixture for two saved books and update test for retrieving a single book

--- tests/test_book_routes.py
+++ tests/test_book_routes.py
@@ -18,3 +18,16 @@ def test_get_all_books_with_no_records(client):
     # Check that the response body is an empty list
     # When no books exist in database, should return []
     assert response_body == []
+
+def test_get_one_book(client, two_saved_books):
+    # Act
+    response = client.get("/books/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "title": "Ocean Book",
+        "description": "watr 4evr"
+    }
