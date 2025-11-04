6b4d3b0be6abe2391f9914cf1722c5cd7a37f14b
Tsz Tin Vivian Soo <tsztin0217@gmail.com>
2025-11-04 18:20:59 -0500

    Enhance author routes to support book creation and retrieval; implement validation and error handling

--- app/routes/author_routes.py
+++ app/routes/author_routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, request, make_response, abort
 from app.models.author import Author
+from app.models.book import Book
 from ..db import db
+from app.routes.route_utilities import validate_model
 
 bp = Blueprint("authors_bp", __name__, url_prefix="/authors")
 
@@ -33,3 +35,29 @@ def get_all_authors():
     authors_response = [author.to_dict() for author in authors]
 
     return authors_response
+
+@bp.post("/<author_id>/books")
+def create_book_with_author(author_id):
+
+    # refer to prior examples or the documentation 
+    # and try completing this endpoint yourself
+    author = validate_model(Author, author_id)
+    request_body = request.get_json()
+    request_body["author_id"] = author.id
+
+    try:
+        new_book = Book.from_dict(request_body)
+    except KeyError as error:
+        response = {"message": f"Invalid request: missing {error.args[0]}"}
+        abort(make_response(response, 400))
+
+    db.session.add(new_book)
+    db.session.commit()
+
+    return make_response(new_book.to_dict(), 201) 
+
+@bp.get("/<author_id>/books")
+def get_books_by_author(author_id):
+    author = validate_model(Author, author_id)
+    response = [book.to_dict() for book in author.books]
+    return response
